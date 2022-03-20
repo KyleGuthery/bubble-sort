23043acd481914a01be2cbb3ff2ed0775025f216
Kyle Guthery <ksguthery@gmail.com>
2022-03-20 10:13:20 -0500

    finished

---
+++ bubble-sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort (array)
+  changed = true
+  while changed == true
+    changed = false
+    array.each_index do |index|
+      if index < array.length-1
+        if array[index] > array[index+1]
+          changed = true
+          array.insert(index, array.delete_at(index+1))
+        end
+      end
+    end
+  end
+  p array
+  return array
+end
+
+bubble_sort([4,3,78,2,0,2])
